d32fc4871d0a80f7bdac02dda1ee3e93138247b8
Mehmet Buyukertas <acizkul6327@gmail.com>
2021-02-02 02:01:42 +0100

    Vytrack Homework

---
+++ src/test/java/pages/CalendarEventsPage.java
@@ -0,0 +1,18 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class CalendarEventsPage extends BasePage{
+
+    @FindBy(xpath = "//div[normalize-space()='Options']")
+    public WebElement options;
+
+    @FindBy(css = "input[type='number']")
+    public WebElement pageNumber;
+
+    @FindBy(xpath = "(//div[@class='btn-group'])[2]")
+    public WebElement viewPerPage;
+
+
+}
